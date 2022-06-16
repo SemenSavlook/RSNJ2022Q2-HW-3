f96812998775e34b6f8796cd54826c78675bbac3
SemenSavlook <semensavluk@gmail.com>
2022-06-16 22:17:03 +0600

    fix: enviornment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RS-NJ-2022-Q2-HW-3
 RSS NJ-2022-Q2 homework#3
+
+<!-- // nodemon --watch './src /**/*.ts' --ext 'ts,json' --exec ts-node .src/index.ts -->
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 const additionT = (a: number, b: number): number => a + b;
 
-const numberR: number = 45;
-const numberRr: number = 10
+const numberR: number = 101;
+const numberRr: number = 1;
 const resulte: number = additionT(numberR, numberRr);
 
+console.log('The result is %d', resulte);
 console.log('The result is %d', resulte);
