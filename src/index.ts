3ab2c88e76ce90b766012c0e8a36ea53eee403dd
SemenSavlook <semensavluk@gmail.com>
2022-06-16 18:55:17 +0600

    create starter env

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const additionT = (a: number, b: number): number => a + b;
+
+const numberR: number = 45;
+const numberRr: number = 10
+const resulte: number = additionT(numberR, numberRr);
+
+console.log('The result is %d', resulte);
